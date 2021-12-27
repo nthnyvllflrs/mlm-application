638120dc89c573e763b2feff93047f7fbc131fdd
nthnyvllflrs <anthonvillaflor@gmail.com>
2021-12-27 10:59:58 +0800

    updated lrvlmdl.cmd

--- lrvlmdl.cmd
+++ lrvlmdl.cmd
@@ -13,11 +13,11 @@ echo Creating Resource ...
 php artisan make:resource %1Resource
 
 echo Creating Form Requests ...
-php artisan make:request %1/%1IndexRequest
-php artisan make:request %1/%1StoreRequest
-php artisan make:request %1/%1ShowRequest
-php artisan make:request %1/%1UpdateRequest
-php artisan make:request %1/%1DestroyRequest
+php artisan make:request %1/Index%1Request
+php artisan make:request %1/Store%1Request
+php artisan make:request %1/Show%1Request
+php artisan make:request %1/Update%1Request
+php artisan make:request %1/Destroy%1Request
 
 echo Creating Factory ...
 php artisan make:factory %1Factory --model=%1
